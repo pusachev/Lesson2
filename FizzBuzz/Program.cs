6d01d0b52971f0d8cfadf78678328cf3bdab1e92
Pavel Usachev <pausachev@gmail.com>
2019-11-13 23:24:54 +0200

    initial

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using FizzBuzzLiblary;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end, fizzNum, buzzNum;
+            string fizzAlias, buzzAlias, userFizz, userBuzz;
+
+            bool exit = false;
+
+            do
+            {
+                fizzNum = 3;
+                buzzNum = 5;
+                fizzAlias = "Fizz";
+                buzzAlias = "Buzz";
+
+                Console.WriteLine("Please enter start number: ");
+
+                if (!int.TryParse(Console.ReadLine(), out start))
+                {
+                    Console.WriteLine("Please enter valid start number");
+                    continue;
+
+                }
+
+                Console.WriteLine("Please enter end number: ");
+
+                if (!int.TryParse(Console.ReadLine(), out end))
+                {
+                    Console.WriteLine("Please enter valid end number");
+                    continue;
+                }
+
+                Console.WriteLine("Please enter fizz number[3]: ");
+
+                string initFizz = Console.ReadLine();
+
+                if (initFizz != "")
+                {
+                    if (!int.TryParse(initFizz, out fizzNum))
+                    {
+
+                        Console.WriteLine("Please enter valid fizz number");
+                        continue;
+                    }
+
+                    if (!FizzBuzzProcessor.isPrime(fizzNum))
+                    {
+                        Console.WriteLine("Please enter only prime num");
+                        continue;
+                    }
+                }
+
+                Console.WriteLine("Please enter buzz number[5]: ");
+
+                string initBuzz = Console.ReadLine();
+
+                if (initBuzz != "")
+                {
+                    if (!int.TryParse(initBuzz, out buzzNum))
+                    {
+
+                        Console.WriteLine("Please enter valid buzz number");
+                        continue;
+                    }
+
+                    if (!FizzBuzzProcessor.isPrime(buzzNum))
+                    {
+                        Console.WriteLine("Please enter only prime num");
+                        continue;
+                    }
+                }
+
+                Console.WriteLine("Please enter fizz alias[Fizz]: ");
+
+                userFizz = Console.ReadLine();
+
+                if (userFizz != "")
+                {
+                    fizzAlias = userFizz;
+                }
+
+                Console.WriteLine("Please enter buzz alias[Buzz]: ");
+
+                userBuzz = Console.ReadLine();
+
+                if (userBuzz != "")
+                {
+                    buzzAlias = userBuzz;
+                }
+
+                Console.WriteLine("Please enter to start");
+
+                FizzBuzzProcessor processor = new FizzBuzzProcessor(start, end, fizzNum, buzzNum, fizzAlias, buzzAlias);
+
+                processor.Process();
+
+                Console.WriteLine("Enter E to key to exit or Enter to continue");
+
+                exit = Console.ReadKey().Key == ConsoleKey.E;
+
+            } while (!exit);
+            
+        }
+    }
+}
---
+++ FizzBuzzLiblary/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace FizzBuzzLiblary
+{
+    public class FizzBuzzProcessor
+    {
+        private readonly int Start;
+        private readonly int End;
+        private readonly int FizzNum;
+        private readonly int BuzzNum;
+        private readonly string FizzAlias;
+        private readonly string BuzzAlias;
+
+        public FizzBuzzProcessor(
+            int start,
+            int end,
+            int fizzNum = 3,
+            int buzzNum = 5,
+            string fizzAlias = "Fizz",
+            string buzzAlias = "Buzz"
+        )
+        {
+            Start       = start;
+            End         = end;
+            FizzNum     = fizzNum;
+            BuzzNum     = buzzNum;
+            FizzAlias   = fizzAlias;
+            BuzzAlias   = buzzAlias;
+
+        }
+
+        public void Process()
+        {
+            int i = Start;
+
+            for (; i < End; i++)
+            { 
+                if (i % FizzNum == 0)
+                {
+                    Console.Write(FizzAlias);
+                }
+
+                if (i % BuzzNum == 0)
+                {
+                    Console.Write(BuzzAlias);
+                }
+
+                if (i % FizzNum != 0 && i % BuzzNum != 0)
+                {
+                    Console.Write(i.ToString());
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static bool isPrime(int num)
+        {
+            bool result = true;
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
